588608c264f777d9b9d155973df31ba42beac563
FranciscoN3to <francisco2012jf@gmail.com>
2020-05-10 18:29:49 -0300

    configurando 404 status

--- src/server.js
+++ src/server.js
@@ -5,13 +5,23 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
+// notfound
+app.use((request, response, next) => {
+
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+
+})
 
 //catch all
 app.use((error, request, response, next) => {
+
     response.status(error.status || 500)
     response.json({error: error.message})
 
     return response
+
 })
 
 app.listen(3333)
