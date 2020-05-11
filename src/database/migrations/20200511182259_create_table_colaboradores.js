81d440a7899a80d29f027a0cce5f24e5d46c25d4
FranciscoN3to <francisco2012jf@gmail.com>
2020-05-11 18:27:35 -0300

    criação migrate colaboradores

---
+++ src/database/migrations/20200511182259_create_table_colaboradores.js
@@ -0,0 +1,28 @@
+
+
+exports.up = knex => knex.schema.createTable('users', (table) => {
+
+    table.increments('id').notNullable().unique().comment('Id do usuario')
+    table.string('nome').notNullable()
+    table.string('sobrenome')
+    table.string('email').notNullable().unique()
+    table.string('celular').unique()
+    table.timestamp('data_nascimento').notNullable()
+    table.string('cpf').notNullable().unique()
+    table.string('cnpj').unique()
+    table.string('rg').unique()
+    table.string('endereco_rua').notNullable()
+    table.string('endereco_numero').notNullable()
+    table.string('endereco_bairro').notNullable()
+    table.string('endereco_cidade').notNullable()
+    table.string('endereco_estado').notNullable()
+    table.string('endereco_cep')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+
+})
+
+
+exports.down = knex => knex.schema.dropTable('users')
+
+
