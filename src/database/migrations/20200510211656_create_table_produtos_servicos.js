407fcd3e43dfab18b7abc86e684a1efac7669a7a
FranciscoN3to <francisco2012jf@gmail.com>
2020-05-11 18:31:48 -0300

    atualizando migrate produtos e serviços

--- src/database/migrations/20200510211656_create_table_produtos_servicos.js
+++ src/database/migrations/20200510211656_create_table_produtos_servicos.js
@@ -4,9 +4,11 @@ exports.up = knex => knex.schema.createTable('produtos_servicos', (table) => {
 
     table.increments('id').notNullable().unique().comment('Id do produto ou servico')
     table.string('nome').notNullable().unique().comment('nome do produto ou servico')
+    table.string('tipo', 15).notNullable().comment('produto / servico')
+    table.string('marca').comment('marca do produto')
     table.decimal('valor', 8, 2).comment('valor do produto ou servico')
-    table.string('tipo', 15).notNullable().comment('produto ou servico')
     table.integer('quantidade').defaultTo(0).comment('quantidade de produto')
+    table.string('descricao').comment('descricao do produto ou servico')
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 
