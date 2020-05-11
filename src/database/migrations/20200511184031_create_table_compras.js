ff4ba474c1f81dda6308ffe34925bb887fec4e62
FranciscoN3to <francisco2012jf@gmail.com>
2020-05-11 19:31:31 -0300

    retirar value default da coluna descrição

--- src/database/migrations/20200511184031_create_table_compras.js
+++ src/database/migrations/20200511184031_create_table_compras.js
@@ -7,7 +7,7 @@ exports.up = knex => knex.schema.createTable('compras', (table) => {
     table.decimal('valor', 8, 2).comment('valor gasto na compra')
     table.integer('quantidade').comment('quantidade comprada')
     table.timestamp('data_compra').notNullable().comment('data da compra')
-    table.text('descricao').defaultTo(0).comment('descricao da compra')
+    table.text('descricao').comment('descricao da compra')
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 
